7e414dd4d620150312e347a849dcc18547c2c2ee
Nicholas Barkero <nsbarkero@cpp.edu>
2025-05-13 00:16:28 -0700

    Add files via upload

--- cs2600_final_pt1.ino
+++ cs2600_final_pt1.ino
@@ -18,7 +18,7 @@ const char* ssid = "SpectrumSetup-FE";
 const char* password = "Ns03@Jm01_Rp05@Jm44";
 
 // IP of the machine running Mosquitto (on your local network)
-const char* mqtt_server = "34.71.227.204";  // Replace with your actual local IP
+const char* mqtt_server = "34.27.138.43";  // Replace with your actual local IP
 unsigned int mqtt_port = 1883;
 
 WiFiClient espClient;
@@ -34,9 +34,9 @@ void setup() {
   lcd.backlight();                // Open the backlight 
 
   lcd.setCursor(0,0);             
-  lcd.print("Local Play:  1");   
+  lcd.print("PvB: 1 PvP: 2");   
   lcd.setCursor(0,1);             
-  lcd.print("Remote Play: 2");    
+  lcd.print("Auto: 3");    
 
   Serial.begin(115200);
 
@@ -59,6 +59,9 @@ void setup() {
     if (mqttClient.connect("ESP32Client")) {
       Serial.println("connected");
       mqttClient.subscribe("esp32/tictac");  // Subscribe to topic
+      mqttClient.subscribe("move");
+      mqttClient.subscribe("counterPlayer");
+      mqttClient.subscribe("counterPlayer2");
 
     } else {
       Serial.print("failed, rc=");
@@ -95,34 +98,43 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
   if (message == "1") { 
     lcd.clear();
-    lcd.setCursor(13,0);
+    lcd.setCursor(5,0);
     lcd.print("1");
     delay(1000);
     lcd.clear();
     delay(1000);
-    lcd.setCursor(13,0);
+    lcd.setCursor(5,0);
     lcd.print("1");
     delay(1000);
     lcd.clear();
-    local();
+    pvb();
   }
 
   else if (message == "2") {
     lcd.clear();
-    lcd.setCursor(13,1);
+    lcd.setCursor(12,0);
     lcd.print("2");
     delay(1000);
     lcd.clear();
     delay(1000);
-    lcd.setCursor(13,1);
+    lcd.setCursor(12,0);
     lcd.print("2");
     delay(1000);
     lcd.clear();
-    remote();
+    pvp(topic, payload, length);
   } 
   else if (message == "3") {
-    Serial.println("Restarting ESP32...");
-    ESP.restart(); // Restart the ESP32
+    lcd.clear();
+    lcd.setCursor(6,1);
+    lcd.print("3");
+    delay(1000);
+    lcd.clear();
+    delay(1000);
+    lcd.setCursor(6,1);
+    lcd.print("3");
+    delay(1000);
+    lcd.clear();
+    autoP();
   }
   else if (message == "4") {
     Serial.println("Stopping program...");
@@ -137,25 +149,360 @@ void loop() {
   mqttClient.loop();
 }
 
-void local() { //note to self: when in different function, does not register callback function inputs
-  int game = 0;
-  int botX = 0;
-  int botO = 0;
-  int tie = 0;
+void pvb() { //note to self: when in different function, does not register callback function inputs
 
-  while (game < 100) {
-    char tic[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
-    int xO = 1; // 1 for X, 0 for O
-    int moves = 0;
-    bool gameOver = false;
+}
+
+void pvp(char* topic, byte* payload, unsigned int length) {
+  int game = 0;
+  bool xDub = false;
+  bool oDub = false;
+  bool tie = false;
+  bool one = false; 
+  bool two = false;
+  bool three = false;
+  bool four = false;
+  bool five = false;
+  bool six = false;
+  bool seven = false;
+  bool eight = false;
+  bool nine = false;
+
+  char tic[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+  int xO = 1; // 1 for X, 0 for O
+  if (xO = 1)
+  {
+  lcd.clear();
+  lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+  lcd.print("PVP: X's Turn");     // The print content is displayed on the LCD 
+  lcd.setCursor(0,1);
+  lcd.print("Pick Space 1-9");
+  mqttClient.publish("counterPlayer", "1");
+  delay(3000);
+  }
+  else if (xO = 0)
+  {
+  lcd.clear();
+  lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+  lcd.print("PVP: O's Turn");     // The print content is displayed on the LCD 
+  lcd.setCursor(0,1);
+  lcd.print("Pick Space 1-9");
+  mqttClient.publish("counterPlayer", "0");
+  delay(3000);
+  }
+
+  int moves = 0;
+  bool gameOver = false;
 
     while (!gameOver && moves < 9) {
       int move;
+      bool choice;
       do {
-        move = random(9);
-      } while (tic[move] != ' ');
+        //move = int val
+          choice = true;
+          Serial.print("Message arrived [");
+          Serial.print(topic);
+          Serial.print("]: ");
 
-      tic[move] = xO ? 'X' : 'O';
+          String message = "";
+          for (int i = 0; i < length; i++) {
+            message += (char)payload[i];
+          }
+          Serial.println(message);
+
+          if (message == "1") { 
+            if(one == true)
+            {
+              choice = false; 
+              if (xO == 1)
+              {
+                lcd.clear();
+                lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+                lcd.print("PVP: X's Turn");     // The print content is displayed on the LCD 
+                lcd.setCursor(0,1);
+                lcd.print("# already in use");
+              }
+              else if (xO == 0)
+              {
+                lcd.clear();
+                lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+                lcd.print("PVP: O's Turn");     // The print content is displayed on the LCD 
+                lcd.setCursor(0,1);
+                lcd.print("# already in use");
+              }
+            }
+            else if (one == false)
+            {
+            move = 1;
+            one = true;
+            mqttClient.publish("move", "1");
+            delay(3000); // Publish every 5 seconds
+            }
+          }
+
+          else if (message == "2") {
+            if(two == true)
+            {
+              choice = false; 
+              if (xO == 1)
+              {
+                lcd.clear();
+                lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+                lcd.print("PVP: X's Turn");     // The print content is displayed on the LCD 
+                lcd.setCursor(0,1);
+                lcd.print("# already in use");
+              }
+              else if (xO == 0)
+              {
+                lcd.clear();
+                lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+                lcd.print("PVP: O's Turn");     // The print content is displayed on the LCD 
+                lcd.setCursor(0,1);
+                lcd.print("# already in use");
+              }
+            }
+            else if (two == false)
+            {
+            move = 2;
+            two = true;
+            mqttClient.publish("move", "2");
+            delay(3000); // Publish every 5 seconds
+            }
+          } 
+          else if (message == "3") {
+            if(three == true)
+            {
+              choice = false; 
+              if (xO == 1)
+              {
+                lcd.clear();
+                lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+                lcd.print("PVP: X's Turn");     // The print content is displayed on the LCD 
+                lcd.setCursor(0,1);
+                lcd.print("# already in use");
+              }
+              else if (xO == 0)
+              {
+                lcd.clear();
+                lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+                lcd.print("PVP: O's Turn");     // The print content is displayed on the LCD 
+                lcd.setCursor(0,1);
+                lcd.print("# already in use");
+              }
+            }
+            else if (three == false)
+            {
+            move = 3;
+            three = true;
+            mqttClient.publish("move", "3");
+            delay(3000); // Publish every 5 seconds
+            }
+          }
+          else if (message == "4") {
+            if(four == true)
+            {
+              choice = false; 
+              if (xO == 1)
+              {
+                lcd.clear();
+                lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+                lcd.print("PVP: X's Turn");     // The print content is displayed on the LCD 
+                lcd.setCursor(0,1);
+                lcd.print("# already in use");
+              }
+              else if (xO == 0)
+              {
+                lcd.clear();
+                lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+                lcd.print("PVP: O's Turn");     // The print content is displayed on the LCD 
+                lcd.setCursor(0,1);
+                lcd.print("# already in use");
+              }
+            }
+            else if (four == false)
+            {
+            move = 4;
+            four = true;
+            mqttClient.publish("move", "4");
+            delay(3000); // Publish every 5 seconds
+            }
+          }
+          else if (message == "5") {
+            if(five == true)
+            {
+              choice = false; 
+              if (xO == 1)
+              {
+                lcd.clear();
+                lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+                lcd.print("PVP: X's Turn");     // The print content is displayed on the LCD 
+                lcd.setCursor(0,1);
+                lcd.print("# already in use");
+              }
+              else if (xO == 0)
+              {
+                lcd.clear();
+                lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+                lcd.print("PVP: O's Turn");     // The print content is displayed on the LCD 
+                lcd.setCursor(0,1);
+                lcd.print("# already in use");
+              }
+            }
+            else if (five == false)
+            {
+            move = 5;
+            five = true;
+            mqttClient.publish("move", "5");
+            delay(3000); // Publish every 5 seconds
+            }
+          }
+          else if (message == "6") {
+            if(six == true)
+            {
+              choice = false; 
+              if (xO == 1)
+              {
+                lcd.clear();
+                lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+                lcd.print("PVP: X's Turn");     // The print content is displayed on the LCD 
+                lcd.setCursor(0,1);
+                lcd.print("# already in use");
+              }
+              else if (xO == 0)
+              {
+                lcd.clear();
+                lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+                lcd.print("PVP: O's Turn");     // The print content is displayed on the LCD 
+                lcd.setCursor(0,1);
+                lcd.print("# already in use");
+              }
+            }
+            else if (six == false)
+            {
+            move = 6;
+            six = true;
+            mqttClient.publish("move", "6");
+            delay(3000); // Publish every 5 seconds
+            }
+          }
+          else if (message == "7") {
+            if(seven == true)
+            {
+              choice = false; 
+              if (xO == 1)
+              {
+                lcd.clear();
+                lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+                lcd.print("PVP: X's Turn");     // The print content is displayed on the LCD 
+                lcd.setCursor(0,1);
+                lcd.print("# already in use");
+              }
+              else if (xO == 0)
+              {
+                lcd.clear();
+                lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+                lcd.print("PVP: O's Turn");     // The print content is displayed on the LCD 
+                lcd.setCursor(0,1);
+                lcd.print("# already in use");
+              }
+            }
+            else if (seven == false)
+            {
+            move = 7;
+            seven = true;
+            mqttClient.publish("move", "7");
+            delay(3000); // Publish every 5 seconds
+            }
+          }
+          else if (message == "8") {
+            if(eight == true)
+            {
+              choice = false; 
+              if (xO == 1)
+              {
+                lcd.clear();
+                lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+                lcd.print("PVP: X's Turn");     // The print content is displayed on the LCD 
+                lcd.setCursor(0,1);
+                lcd.print("# already in use");
+              }
+              else if (xO == 0)
+              {
+                lcd.clear();
+                lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+                lcd.print("PVP: O's Turn");     // The print content is displayed on the LCD 
+                lcd.setCursor(0,1);
+                lcd.print("# already in use");
+              }
+            }
+            else if (eight == false)
+            {
+            move = 8;
+            eight = true;
+            mqttClient.publish("move", "8");
+            delay(3000); // Publish every 5 seconds
+            }
+          }
+          else if (message == "9") {
+            if(nine == true)
+            {
+              choice = false; 
+              if (xO == 1)
+              {
+                lcd.clear();
+                lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+                lcd.print("PVP: X's Turn");     // The print content is displayed on the LCD 
+                lcd.setCursor(0,1);
+                lcd.print("# already in use");
+              }
+              else if (xO == 0)
+              {
+                lcd.clear();
+                lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+                lcd.print("PVP: O's Turn");     // The print content is displayed on the LCD 
+                lcd.setCursor(0,1);
+                lcd.print("# already in use");
+              }
+            }
+            else if (nine == false)
+            {
+            move = 9;
+            nine = true;
+            mqttClient.publish("move", "9");
+            delay(3000); // Publish every 5 seconds
+            }
+          }
+          else
+          {
+            Serial.println("Invalid input received");
+            choice == false;
+            if (xO == 1)
+              {
+                lcd.clear();
+                lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+                lcd.print("PVP: X's Turn");     // The print content is displayed on the LCD 
+                lcd.setCursor(0,1);
+                lcd.print("Invalid");
+              }
+              else if (xO == 0)
+              {
+                lcd.clear();
+                lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+                lcd.print("PVP: O's Turn");     // The print content is displayed on the LCD 
+                lcd.setCursor(0,1);
+                lcd.print("Invalid");
+              }
+          }
+      } while (tic[move - 1] != ' ' && choice == false);
+
+      if (xO == 1)
+      {
+        tic[move-1] = 'X';
+      }
+      else if (xO == 0)
+      {
+        tic[move-1] = 'O';
+      }
       moves++;
 
       // Check for win
@@ -168,15 +515,15 @@ void local() { //note to self: when in different function, does not register cal
           (tic[0] == tic[4] && tic[4] == tic[8] && tic[0] != ' ') ||
           (tic[2] == tic[4] && tic[4] == tic[6] && tic[2] != ' ')) {
         gameOver = true;
-        if (xO == 1) botX++;
-        else if (xO == 0) botO++;
+        if (xO == 1) xDub = true;
+        else if (xO == 0) oDub = true;
         game++;
       }
 
       if (moves == 9 && gameOver == false) 
       {
         gameOver = true;
-        tie++;
+        tie = true;
         game++;
       }
       if(xO == 1) // Switch turn
@@ -188,29 +535,38 @@ void local() { //note to self: when in different function, does not register cal
         xO = 1;
       }
     }
-
-
-    // Display result on LCD
-    lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
-    lcd.print("Bot v Bot");     // The print content is displayed on the LCD 
-    lcd.print(" G:");
-    lcd.print(game);
-    lcd.setCursor(0, 1);
-    lcd.print("X:");
-    lcd.print(botX);
-    lcd.print(" O:");
-    lcd.print(botO);
-    lcd.print(" T:");
-    lcd.print(tie);
-    delay(5000); // Delay to view result
-  } 
-  while(game == 100)
-  {
-    delay(1);
-  }
+    if (xDub == true)
+    {
+      lcd.clear();
+      lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+      lcd.print("X WON! Congrats!");     // The print content is displayed on the LCD 
+      lcd.setCursor(0,1);
+      lcd.print("Returning to menu...");
+      delay(7000);
+      setup();
+    }
+    else if (oDub == true)
+    {
+      lcd.clear();
+      lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+      lcd.print("O WON! Congrats!");     // The print content is displayed on the LCD 
+      lcd.setCursor(0,1);
+      lcd.print("Returning to menu...");
+      delay(7000);
+      setup();
+    }
+    else if (tie == true)
+    {
+      lcd.clear();
+      lcd.setCursor(0,0);             // Move the cursor to row 0, column 0 
+      lcd.print("DRAW!!");     // The print content is displayed on the LCD 
+      lcd.setCursor(0,1);
+      lcd.print("Returning to menu...");
+      delay(7000);
+      setup();
+    }
 }
-
-void remote() {
+void autoP() {
 
 }
 
@@ -221,3 +577,4 @@ bool i2CAddrTest(uint8_t addr) {
   } 
   return false; 
 }
+
